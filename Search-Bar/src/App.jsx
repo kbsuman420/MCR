37572b6d8789ffa3882ea7423e960e8ddfbfd69e
Suman Kundu <kundu.1suman@gmail.com>
2026-04-04 19:41:00 +0530

    Search bar Challenge Coompleted

--- Search-Bar/src/App.jsx
+++ Search-Bar/src/App.jsx
@@ -1,121 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function APP() {
+  const [searchItem, setSearchItem] = useState("");
+  const [recipes, setRecipes] = useState([])
+  const [error, setError] = useState("");
+  const [cache, setCache] = useState({})
+  const [isFocus, setIsFocus] =  useState(false);
+
+  const fetchData = async () => {
+
+    if(cache[searchItem]) {
+      console.log("Returnn data from cache")
+      return setRecipes(cache[searchItem]);
+    }
+    console.log(searchItem)
+    const data = await fetch(`https://dummyjson.com/recipes/search?q=${searchItem}`);
+    await data.json().then((data) => {
+      setRecipes(data.recipes)
+      setCache((prev) => ({...prev, [searchItem]: data.recipes}))
+    }
+    ).catch((err) => setError(err))
+    
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(fetchData, 400);
+
+    return () => clearTimeout(timer)
+  }, [searchItem])
+
+
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="container">
+      <div className="search-container">
+        <input type="text" className="input" 
+        placeholder="Search your url" value={searchItem} 
+        onChange={(e) => setSearchItem(e.target.value)} 
+        onFocus={() => setIsFocus((prev) => !prev)}
+        onBlur={() => setIsFocus((prev) => !prev)}
+        />
 
-      <div className="ticks"></div>
+        {isFocus && <div className="search-hints">
+          {recipes.map((recipe) => (
+            <p className="items" key={recipe.id} onMouseDown={() => 
+              setSearchItem(recipe.name)
+            }>{recipe.name}</p>
+          ))}
+        </div>}
+          {error && <p>Network not responding</p>}
+      </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    </div>
   )
 }
-
-export default App
