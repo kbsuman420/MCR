5e65183b8e9bd8fbcd1eb313090723446cb0bc7e
Suman Kundu <kundu.1suman@gmail.com>
2026-04-05 22:58:30 +0530

    Nested Checked Box

--- Nested-Check-Box/src/App.jsx
+++ Nested-Check-Box/src/App.jsx
@@ -1,121 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react"
+import json from "./data.json"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Checkbox = ({data, setList, list}) => {
+
+  const handleCheckBoxes = (list, id) => {
+    console.log(list)
+    const updateData = (listArr) => {
+      const ud = listArr.map((lst) => {
+        console.log(lst)
+        if(lst.id === id) {
+          return {...lst, checked: !lst.checked}
+        }
+        if(lst.children.length) {
+          return {...lst, children: updateData(lst.children)}
+        }
+        return lst;
+      })
+      return ud;
+    }
+    setList(updateData(list))
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div className="checkbox">
+      <label>
+        <input type="checkbox" name={data.name} checked={data.checked} onChange={() => handleCheckBoxes(list, data.id)}/>
+        {data.name}
+      </label>
+      {data.children && data.children.map((d1) => <Checkbox data={d1} key={d1.id} setList={setList} list={list}/>)}
+    </div>
   )
 }
 
-export default App
+export default function App() {
+  const [list, setList] = useState(json);
+
+  return (
+    <div className="container">
+      {
+        list.map((data) => (
+          <Checkbox key={data.id} data={data} setList={setList} list={list}/>
+        ))
+      }
+    </div>
+  )
+}
